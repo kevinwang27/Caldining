33afa3316aad4ac8d2506a4f549bfe16aa2d0f19
Kevin <kevinwang27@berkeley.edu>
2018-03-22 22:53:57 -0700

    before one single menu fragment

--- app/src/main/java/MenuFragments/FoothillFragment.java
+++ app/src/main/java/MenuFragments/FoothillFragment.java
@@ -5,17 +5,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import kevinwang.personal.caldining.R;
 
 public class FoothillFragment extends Fragment {
+
+    private LinearLayout mBreakfastMenu;
+    private LinearLayout mBreakfastContainer;
+    private LinearLayout mLunchContainer;
+    private LinearLayout mDinnerContainer;
 
     public FoothillFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_foothill, container, false);
+        View v = inflater.inflate(R.layout.fragment_foothill, container, false);
+        mBreakfastMenu = v.findViewById(R.id.foothill_breakfast_menu);
+        mBreakfastContainer = v.findViewById(R.id.foothill_breakfast_container);
+        mLunchContainer = v.findViewById(R.id.foothill_lunch_container);
+        mDinnerContainer = v.findViewById(R.id.foothill_dinner_container);
+
+        return v;
     }
 
 }
