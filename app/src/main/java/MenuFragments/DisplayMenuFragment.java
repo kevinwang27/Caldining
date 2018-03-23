1c3c7b943535c2b23a544afb337a776604db2e42
Kevin <kevinwang27@berkeley.edu>
2018-03-23 11:33:15 -0700

    added station titles and foods

--- app/src/main/java/MenuFragments/DisplayMenuFragment.java
+++ app/src/main/java/MenuFragments/DisplayMenuFragment.java
@@ -1,10 +1,10 @@
 package MenuFragments;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +17,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import kevinwang.personal.caldining.MenusFragment;
 import kevinwang.personal.caldining.R;
@@ -24,10 +27,9 @@ import kevinwang.personal.caldining.R;
 public class DisplayMenuFragment extends Fragment {
 
     private LinearLayout mMenuContainer;
+    private LinearLayout mSecondMenu;
     private LinearLayout mThirdMenu;
-    private LinearLayout mFirstContainer;
-    private LinearLayout mSecondContainer;
-    private LinearLayout mThirdContainer;
+    private LinearLayout[] mContainers;
     private TextView mFirstTitle;
     private TextView mSecondTitle;
     private TextView mThirdTitle;
@@ -48,11 +50,13 @@ public class DisplayMenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_display_menu, container, false);
+        mContainers = new LinearLayout[3];
         mMenuContainer = v.findViewById(R.id.menu_container);
+        mSecondMenu = v.findViewById(R.id.second_menu);
         mThirdMenu = v.findViewById(R.id.third_menu);
-        mFirstContainer = v.findViewById(R.id.first_container);
-        mSecondContainer = v.findViewById(R.id.second_container);
-        mThirdContainer = v.findViewById(R.id.third_container);
+        mContainers[0] = v.findViewById(R.id.first_container);
+        mContainers[1] = v.findViewById(R.id.second_container);
+        mContainers[2] = v.findViewById(R.id.third_container);
         mFirstTitle = v.findViewById(R.id.first_title);
         mSecondTitle = v.findViewById(R.id.second_title);
         mThirdTitle = v.findViewById(R.id.third_title);
@@ -97,30 +101,94 @@ public class DisplayMenuFragment extends Fragment {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                final ArrayList<String> titles = new ArrayList<>();
+                final List<List<String>> stationLists = new ArrayList<>();
+                final List<List<List<String>>> foodListofLists = new ArrayList<>();
                 try {
                     Document document = Jsoup.connect(url).get();
                     Element todayMenus = document.getElementsByClass("menu_wrap_overall").first();
                     Elements mealTitles = todayMenus.getElementsByClass("location_period");
-                    if (mealTitles.size() == 3) {
-                        for (Element title : mealTitles) {
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    
-                                }
-                            });
-                        }
+                    for (Element title : mealTitles) {
+                        titles.add(title.text());
                     }
                     Elements menus = todayMenus.getElementsByClass("desc_wrap_ck3");
-                    for (Element menu : menus) {
-                        Elements stations = menu.getElementsByClass("station_wrap");
-
+                    for (int numMenus = 0; numMenus < menus.size(); numMenus++) {
+                        stationLists.add(new ArrayList<String>());
+                        foodListofLists.add(new ArrayList<List<String>>());
+                        Elements mealStations = menus.get(numMenus).getElementsByClass("station_wrap");
+                        for (int numStations = 0; numStations < mealStations.size(); numStations++) {
+                            stationLists.get(numMenus).add(mealStations.get(numStations).text());
+                            foodListofLists.get(numMenus).add(new ArrayList<String>());
+                            Elements mealFoods = mealStations.get(numStations).siblingElements();
+                            for (Element food : mealFoods) {
+                                foodListofLists.get(numMenus).get(numStations).add(food.text());
+                            }
+                        }
                     }
                 } catch (IOException e) {
                     buildErrorAlert();
                 }
+
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setTitles(titles);
+                        for (int i = 0; i < stationLists.size(); i++) {
+                            createStations(mContainers[i], stationLists.get(i), foodListofLists.get(i));
+                        }
+                    }
+                });
             }
         }).start();
+    }
+
+    private void setTitles(ArrayList<String> titles) {
+        switch (titles.size()) {
+            case 1:
+                mFirstTitle.setText(titles.get(0));
+                mMenuContainer.removeView(mSecondMenu);
+                mMenuContainer.removeView(mThirdMenu);
+                break;
+            case 2:
+                mFirstTitle.setText(titles.get(0));
+                mSecondTitle.setText(titles.get(1));
+                mMenuContainer.removeView(mThirdMenu);
+                break;
+            case 3:
+                mFirstTitle.setText(titles.get(0));
+                mSecondTitle.setText(titles.get(1));
+                mThirdTitle.setText(titles.get(2));
+                break;
+            default:
+                //numMenus = 0;
+                //weird menu??
+        }
+    }
+
+    private void createStations(LinearLayout container, List<String> stations, List<List<String>> foodList) {
+        for (int i = 0; i < stations.size(); i++) {
+            LinearLayout menuStation = new LinearLayout(getContext());
+            menuStation.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            menuStation.setLayoutParams(params);
+
+            TextView stationTitle = new TextView(getContext());
+            stationTitle.setTextColor(Color.BLACK);
+            stationTitle.setText(stations.get(i));
+            container.addView(stationTitle);
+
+            setFoods(menuStation, foodList.get(i));
+        }
+    }
+
+    private void setFoods(LinearLayout container, List<String> foods) {
+        for (String food : foods) {
+            TextView foodText = new TextView(getContext());
+            foodText.setTextColor(Color.BLACK);
+            foodText.setText(food);
+            container.addView(foodText);
+        }
     }
 
 }
